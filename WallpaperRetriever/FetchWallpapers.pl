edf6832b1f01c73193e463463300d35c1e9e0813
unknown <daemonstorm@gmail.com>
2012-05-28 19:55:01 -0700

    started adding comments to the short scripts to make them more understandable

--- WallpaperRetriever/FetchWallpapers.pl
+++ WallpaperRetriever/FetchWallpapers.pl
@@ -7,6 +7,33 @@ use threads;
 use Thread::Queue;
 use threads::shared;
 
+=head1 NAME
+
+FetchWallpapers - A program to find and download all the wallpapers from
+http://wall.alphacoders.com
+
+=head1 SYNOPSIS
+
+FetchWallpapers uses Mechanize and Perl threads to scrape through the Alphacoders
+website to grab all the images that people have uploaded to it. The methodology
+used by this program allows it to find pages without making any assumptions from
+the initial launch. It starts by reading the homepage and from there spiders out
+to find more pages and links based on what is linked on the homepage at the time.
+
+=head1 DESCRIPTION
+
+=head1 COPYRIGHT
+
+Copyright 2012 Kevin Brown.
+
+Permission is granted to copy, distribute and/or modify this 
+document under the terms of the GNU Free Documentation 
+License, Version 2.0 or any later version published by the 
+Free Software Foundation; with no Invariant Sections, with 
+no Front-Cover Texts, and with no Back-Cover Texts.
+
+=cut
+
 my $artist_queue = Thread::Queue->new();
 my $image_queue  = Thread::Queue->new();
 
@@ -30,6 +57,14 @@ while ( $art_thread->is_running() || $image_thread->is_running() ) {
 	sleep(30);
 }
 
+=head2 artist_pages
+
+When an artist profile is enqueued, artist_pages parses it to find other artists
+and wallpaper images by looking at the favorites list of that artist, the people
+who have left comments on that page and the actual artwork uploaded by that 
+artist.
+
+=cut
 sub artist_pages {
 
 	my $art_agent = WWW::Mechanize->new(
@@ -61,6 +96,12 @@ sub artist_pages {
 	}
 }
 
+=head2 image_pages
+
+image_pages enqueues new found wallpapers and artists by checking queued images 
+for comments and other favorited images.
+
+=cut
 sub image_pages {
 	my $image_agent = WWW::Mechanize->new(
 		agent       => "Linux Mozilla",
@@ -105,6 +146,15 @@ sub image_pages {
 	}
 }
 
+=head2
+
+enqueue_links reads through the page content and queues up the found links from
+the page based on two patterns. One pattern is for actual wallpaper pages, the 
+other is for artist pages. Before queueing up the link, it checks a shared array
+to see if that page has already been dug through before. This reduces some 
+redundancy that the program might accidently go through otherwise.
+
+=cut
 sub enqueue_links {
 	my $agent = shift;
 	if ( $agent->success() ) {
